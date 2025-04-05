0e5905c038ff6cc1a90aeebd4622e45b87a06b4a
Minyoung <alsendrha@naver.com>
2025-04-05 15:45:26 +0900

    설정 버전 env적용

--- src/screen/myPage/SettingPage.tsx
+++ src/screen/myPage/SettingPage.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import Config from 'react-native-config';
 import {SWidth} from '../../../globalStyle';
 import SettingItem from '../../components/MyPage/Settings/SettingItem';
-
 const SettingPage = () => {
   const settingList = [
     {id: 1, title: '온보딩', icon: true, onPress: () => {}},
     {id: 2, title: '약관 및 정책', icon: true, onPress: () => {}},
-    {id: 3, title: '버전 정보 v.1.0.0', icon: false, onPress: () => {}},
+    {
+      id: 3,
+      title: `버전 정보 v.${Config.APP_VERSION}`,
+      icon: false,
+      onPress: () => {},
+    },
     {id: 4, title: '탈퇴하기', icon: false, onPress: () => {}},
     {id: 5, title: '로그아웃', icon: false, onPress: () => {}},
     {id: 6, title: '캐시 데티터 삭제', icon: false, onPress: () => {}},
